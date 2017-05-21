213b621aff27cc271d8b7cc68d8d51e467db309a
worron <worrongm@gmail.com>
2017-05-22 01:19:52 +0300

    Add development branch checker to version function

--- aniwall/version.py
+++ aniwall/version.py
@@ -4,6 +4,8 @@ import subprocess
 
 from aniwall.logger import logger
 
+# noinspection SpellCheckingInspection
+_DEVELOPMENT_BRANCH = "devel"
 _FALLBACK_VERSION = 0.8
 _version_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "version")
 
@@ -15,16 +17,21 @@ def get_current():
 	"""
 	version = _FALLBACK_VERSION
 	try:
-		output = subprocess.check_output(
-			["git", "describe", "--tags", "--long"],
-			stderr=subprocess.PIPE,
-			cwd=os.path.dirname(os.path.abspath(__file__))
-		)
+		cwd_ = os.path.dirname(os.path.abspath(__file__))
+
+		output = subprocess.check_output(["git", "describe", "--tags", "--long"], stderr=subprocess.PIPE, cwd=cwd_)
 		version = str(output, "utf-8").strip()
+		output = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], stderr=subprocess.PIPE, cwd=cwd_)
+		branch = str(output, "utf-8").strip()
 
 		commit_num = re.search("-(\d)", version).group(1)
 		if commit_num == "0":
 			version = version.split('-')[0]
+		elif branch == _DEVELOPMENT_BRANCH:
+			vl = version.split('-')
+			vl[0] = ("%.1f" % (float(vl[0]) + 0.1)) + ".dev"
+			version = "-".join(vl)
+
 	except Exception as e:
 		logger.debug("Can't read git output:\n%s", e)
 
