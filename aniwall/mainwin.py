5a61ad0437561a0d4d3a068ac8e42edb6c02b659
worron <worrongm@gmail.com>
2017-05-22 20:50:58 +0300

    Add gui tooltips

--- aniwall/mainwin.py
+++ aniwall/mainwin.py
@@ -8,7 +8,6 @@ from aniwall.common import TreeViewData, GuiBase, hex_from_rgba, rgba_from_hex, 
 # TODO: color moving inside palette
 # TODO: respect image aspect option
 # TODO: More GUI settings
-# TODO: GUI tooltips
 # TODO: GUI translation (?)
 
 
