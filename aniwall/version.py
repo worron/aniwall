11cac94447abf34a19b188de008ff788abad9a13
worron <worrongm@gmail.com>
2017-05-27 22:35:16 +0300

    Update version

--- aniwall/version.py
+++ aniwall/version.py
@@ -3,7 +3,7 @@ import subprocess
 
 from aniwall.logger import logger
 
-_FALLBACK_VERSION = 0.9
+_FALLBACK_VERSION = 1.0
 # noinspection SpellCheckingInspection
 _DEVELOPMENT_BRANCH = "devel"
 _MASTER_BRANCH = "master"
